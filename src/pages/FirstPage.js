1b00aeda48aceeab13d855d917fb4d36e3042aaf
VipiinS <Vipinshanmugam@gmail.com>
2023-01-08 19:54:23 +0530

    changed firstPage to mui gird

--- src/pages/FirstPage.js
+++ src/pages/FirstPage.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { useSelector } from "react-redux";
 import AnimeBox from '../components/AnimeBox/AnimeBox';
-
+import { Container, Grid } from '@material-ui/core';
+import useStyles from './styles';
 
 
 
 const FirstPage = () => {
-  
+  const classes = useStyles();
     const {animeItems} = useSelector((store)=>(store.anime));
 
       const Elements = animeItems.map((item)=>{
@@ -27,7 +28,11 @@ const FirstPage = () => {
         (
     <div className="container">
       <div className="App">
-        {Elements}
+        <Container className={classes.cardGrid} maxWidth='md'>
+          <Grid container spacing={2}>
+          {Elements}
+          </Grid>
+        </Container>
       </div>
     </div>
   )
